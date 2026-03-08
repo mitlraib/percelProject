e042eea7b7e28daa094270a1f3222065026f03f6
mitlraib <92878071+mitlraib@users.noreply.github.com>
2026-03-08 15:02:51 +0200

    Fix mobile scaling issue

--- src/client/game/main.ts
+++ src/client/game/main.ts
@@ -6,14 +6,21 @@ import ParallaxScene from "./scenes/ParallaxScene";
 export default function startGame() {
   const config: Phaser.Types.Core.GameConfig = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 520,
     parent: "game-container",
     backgroundColor: "#222222",
+
+    scale: {
+      mode: Phaser.Scale.FIT,
+      autoCenter: Phaser.Scale.CENTER_BOTH,
+      width: 800,
+      height: 520,
+    },
+
     physics: {
       default: "arcade",
       arcade: { gravity: { x: 0, y: 200 }, debug: false },
     },
+
     scene: [MenuScene, NetworkScene, ParallaxScene],
   };
 
--- src/client/main.ts
+++ src/client/main.ts
@@ -1,3 +1,5 @@
+//src\client\main.ts
+
 import startGame from "./game/main";
 
 window.addEventListener("load", () => {
